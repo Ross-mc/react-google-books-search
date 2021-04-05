80de1e2b509a6a1e5f067c6de612a250cec77059
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-04-05 15:00:27 +0100

    mongoose schema added for books

---
+++ models/books.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const BooksSchema = new Schema({
+  books: [
+    {
+      title: {
+        type: String,
+        required: "Title is required"
+      }
+    },
+    {
+      authors: {
+        type: Array,
+        required: "An array of authors is required (a single author should still be provided in an array)"
+      }
+    },
+    {
+      description: {
+        type: String,
+        required: "Description is required"
+      }
+    },
+    {
+      image: {
+        type: String,
+        default: "https://specials-images.forbesimg.com/imageserve/5f85be4ed0acaafe77436710/960x0.jpg"
+      }
+    },
+    {
+      link: {
+        type: String,
+        required: "Link is required"
+      }
+    },
+  ]
+})
+
+const Books = mongoose.model("books", BooksSchema)
+
+module.exports = Books
