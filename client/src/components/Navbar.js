2fa5b01b07fc347fb7c3cdf53897332afb85d817
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-04-11 12:58:32 +0100

    changed navbar layout

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,38 +1,51 @@
-import {Link, useLocation} from "react-router-dom"
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
-  const currentPageClass = "nav-item nav-link active";
-  const notCurrentPageClass = "nav-item nav-link";
-  const location = useLocation()
+  const currentPageClass = "nav-item nav-link text-dark";
+  const notCurrentPageClass = "nav-item nav-link text-muted";
+  const location = useLocation();
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-sm bg-light">
       <Link className="navbar-brand" to="/">
         Google Books Search
       </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
-          <Link className={location.pathname === "/" ? currentPageClass : notCurrentPageClass } to="/">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link
+            className={
+              location.pathname === "/" ? currentPageClass : notCurrentPageClass
+            }
+            to="/"
+          >
             Home
           </Link>
-          <Link className={location.pathname === "/search" ? currentPageClass : notCurrentPageClass } to="/search">
-          Search Books
+        </li>
+        <li className="nav-item">
+          <Link
+            className={
+              location.pathname === "/search"
+                ? currentPageClass
+                : notCurrentPageClass
+            }
+            to="/search"
+          >
+            Search Books
           </Link>
-          <Link className={location.pathname === "/saved" ? currentPageClass : notCurrentPageClass } to="/saved">
-          Saved Books
+        </li>
+        <li className="nav-item">
+          <Link
+            className={
+              location.pathname === "/saved"
+                ? currentPageClass
+                : notCurrentPageClass
+            }
+            to="/saved"
+          >
+            Saved Books
           </Link>
-        </div>
-      </div>
+        </li>
+      </ul>
     </nav>
   );
 };
