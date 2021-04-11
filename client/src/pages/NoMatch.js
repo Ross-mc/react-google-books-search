73fccb17d366c7fdb300b31b8253c49982749210
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-04-11 13:11:53 +0100

    created no match page

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,7 +1,26 @@
+import {Link} from "react-router-dom"
+
 const NoMatch = () => {
   return (
-    <h1>Hello from the NoMatch page</h1>
-  )
+    <div className="jumbotron">
+      <h1 className="display-4">Ooooops!</h1>
+      <p className="lead">
+      We couldn't find the page you were looking for
+      </p>
+      <hr className="my-4" />
+      <p>
+        Try these links below!
+      </p>
+      <p className="lead">
+        <Link className="btn btn-primary btn-lg" to="/search" role="button">
+          Search Books
+        </Link>
+        <Link className="btn btn-primary btn-lg" to="/saved" role="button">
+          Saved Books
+        </Link>
+      </p>
+    </div>
+  );
 };
 
 export default NoMatch
