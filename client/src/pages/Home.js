70b1a762a4d175898f7a94ab98f8dc9165514ef0
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-04-11 13:09:34 +0100

    added home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,26 @@
+import {Link} from "react-router-dom"
+
 const Home = () => {
   return (
-    <h1>Hello from the home page</h1>
-  )
+    <div className="jumbotron">
+      <h1 className="display-4">Google Books Search</h1>
+      <p className="lead">
+        An application for searching a storing books found through the Google Books API.
+      </p>
+      <hr className="my-4" />
+      <p>
+        Select from the links below to search for books or see books already saved in our database
+      </p>
+      <p className="lead">
+        <Link className="btn btn-primary btn-lg" to="/search" role="button">
+          Search Books
+        </Link>
+        <Link className="btn btn-primary btn-lg" to="/saved" role="button">
+          Saved Books
+        </Link>
+      </p>
+    </div>
+  );
 };
 
-export default Home
+export default Home;
